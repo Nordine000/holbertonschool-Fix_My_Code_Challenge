945748dadd3ef5739df283ab2b355d9a3b6cf5f8
Nordine000 <ounissi970@gmail.com>
2025-09-26 02:27:33 +0200

    test

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -14,11 +14,12 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2], 10)
+const size = parseInt(process.argv[2], 10);
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
-        process.stdout.write("#");
+if (isNaN(size)) {
+    console.log('Missing size');
+} else {
+    for (let i = 0; i < size; i++) {
+        console.log('#'.repeat(size));
     }
-    process.stdout.write("\n");
 }
